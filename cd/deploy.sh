c2e0170899854992b2be9e112c48c1990e69e8c6
hippoom <hippoom@gmail.com>
2017-06-27 23:20:22 +0800

    Exclude demo application from deploying to maven central

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_95de03115781_key -iv $encrypted_95de03115781_iv -in cd/codesigning.asc.enc -out cd/signingkey.asc -d
 gpg --fast-import cd/signingkey.asc
-mvn deploy -P ossrh --settings cd/mvn.settings.xml
+mvn deploy -P ossrh --projects !wechat-mp-starter-demo --settings cd/mvn.settings.xml
