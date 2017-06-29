d2b4eba8515cb25c1b18c581e4eb5cef00412534
hippoom <hippoom@gmail.com>
2017-06-29 17:49:27 +0800

    Provide derived services from WxMpService for convenience

--- wechat-mp-autoconfigure/src/main/java/com/github/hippoom/wechat/mp/autoconfigure/WeChatMpConfiguration.java
+++ wechat-mp-autoconfigure/src/main/java/com/github/hippoom/wechat/mp/autoconfigure/WeChatMpConfiguration.java
@@ -2,11 +2,21 @@ package com.github.hippoom.wechat.mp.autoconfigure;
 
 import com.github.hippoom.wechat.mp.autoconfigure.web.WeChatMpWebConfiguration;
 import com.github.hippoom.wechat.mp.autoconfigure.web.WeChatMpWebMethodConfiguration;
+import me.chanjar.weixin.mp.api.WxMpCardService;
 import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpDataCubeService;
+import me.chanjar.weixin.mp.api.WxMpDeviceService;
 import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
 import me.chanjar.weixin.mp.api.WxMpKefuService;
+import me.chanjar.weixin.mp.api.WxMpMaterialService;
+import me.chanjar.weixin.mp.api.WxMpMenuService;
+import me.chanjar.weixin.mp.api.WxMpQrcodeService;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpStoreService;
+import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
+import me.chanjar.weixin.mp.api.WxMpUserBlacklistService;
 import me.chanjar.weixin.mp.api.WxMpUserService;
+import me.chanjar.weixin.mp.api.WxMpUserTagService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,6 +24,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ * Provide default {@link WxMpService}.
+ *
+ * @see WeChatMpProperties
+ * @see WeChatMpWebConfiguration
+ * @see WeChatMpWebMethodConfiguration
+ * @since 0.1.0
+ */
 @Configuration
 @EnableConfigurationProperties(WeChatMpProperties.class)
 @Import(
@@ -24,6 +42,14 @@ import org.springframework.context.annotation.Import;
 )
 public class WeChatMpConfiguration {
 
+    /**
+     * Default in memory {@link WxMpConfigStorage}, you can provide a custom instance, see <a
+     * href="https://github.com/Hippoom/wechat-mp-starter/wiki/Autowiring-WxMpConfigStorage">wiki</a>
+     * for detail.
+     *
+     * @see WxMpConfigStorage
+     * @see WeChatMpProperties
+     */
     @Bean
     @ConditionalOnMissingBean
     protected WxMpConfigStorage configStorage(WeChatMpProperties weChatMpProperties) {
@@ -35,6 +61,13 @@ public class WeChatMpConfiguration {
         return configStorage;
     }
 
+    /**
+     * Default {@link WxMpService}, you can provide a custom instance, see <a
+     * href="https://github.com/Hippoom/wechat-mp-starter/wiki/Autowiring-WxMpService">wiki</a>
+     * for detail.
+     *
+     * @see WxMpService
+     */
     @Bean
     @ConditionalOnMissingBean
     protected WxMpService wxMpService(WxMpConfigStorage wxMpConfigStorage) {
@@ -43,13 +76,123 @@ public class WeChatMpConfiguration {
         return wxMpService;
     }
 
+    /**
+     * Default {@link WxMpKefuService} provider.
+     *
+     * @see WxMpKefuService
+     */
     @Bean
     protected WxMpKefuService wxMpKefuService(WxMpService wxMpService) {
         return wxMpService.getKefuService();
     }
 
+    /**
+     * Default {@link WxMpUserService} provider.
+     *
+     * @see WxMpUserService
+     */
     @Bean
     protected WxMpUserService wxMpUserService(WxMpService wxMpService) {
         return wxMpService.getUserService();
+    }
+
+    /**
+     * Default {@link WxMpUserTagService} provider.
+     *
+     * @see WxMpUserTagService
+     */
+    @Bean
+    protected WxMpUserTagService wxMpUserTagService(WxMpService wxMpService) {
+        return wxMpService.getUserTagService();
+    }
+
+    /**
+     * Default {@link WxMpMenuService} provider.
+     *
+     * @see WxMpMenuService
+     */
+    @Bean
+    protected WxMpMenuService wxMpMenuService(WxMpService wxMpService) {
+        return wxMpService.getMenuService();
+    }
+
+    /**
+     * Default {@link WxMpMaterialService} provider.
+     *
+     * @see WxMpMaterialService
+     */
+    @Bean
+    protected WxMpMaterialService wxMpMaterialService(WxMpService wxMpService) {
+        return wxMpService.getMaterialService();
+    }
+
+    /**
+     * Default {@link WxMpQrcodeService} provider.
+     *
+     * @see WxMpQrcodeService
+     */
+    @Bean
+    protected WxMpQrcodeService wxMpQrcodeService(WxMpService wxMpService) {
+        return wxMpService.getQrcodeService();
+    }
+
+    /**
+     * Default {@link WxMpTemplateMsgService} provider.
+     *
+     * @see WxMpTemplateMsgService
+     */
+    @Bean
+    protected WxMpTemplateMsgService wxMpTemplateMsgService(WxMpService wxMpService) {
+        return wxMpService.getTemplateMsgService();
+    }
+
+    /**
+     * Default {@link WxMpUserBlacklistService} provider.
+     *
+     * @see WxMpUserBlacklistService
+     */
+    @Bean
+    protected WxMpUserBlacklistService wxMpUserBlacklistService(WxMpService wxMpService) {
+        return wxMpService.getBlackListService();
+    }
+
+    /**
+     * Default {@link WxMpCardService} provider.
+     *
+     * @see WxMpCardService
+     */
+    @Bean
+    protected WxMpCardService wxMpCardService(WxMpService wxMpService) {
+        return wxMpService.getCardService();
+    }
+
+    /**
+     * Default {@link WxMpDeviceService} provider.
+     *
+     * @see WxMpDeviceService
+     */
+    @Bean
+    protected WxMpDeviceService wxMpDeviceService(WxMpService wxMpService) {
+        return wxMpService.getDeviceService();
+    }
+
+    /**
+     * Default {@link WxMpDataCubeService} provider.
+     *
+     * @see WxMpDataCubeService
+     */
+    @Bean
+    protected WxMpDataCubeService wxMpDataCubeService(WxMpService wxMpService) {
+        return wxMpService.getDataCubeService();
+    }
+
+    /**
+     * Default {@link WxMpStoreService} provider.
+     *
+     * @see WxMpStoreService
+     */
+    @Bean
+    protected WxMpStoreService wxMpStoreService(WxMpService wxMpService) {
+        return wxMpService.getStoreService();
     }
 }
--- wechat-mp-autoconfigure/src/main/java/com/github/hippoom/wechat/mp/autoconfigure/annotation/EnableWeChatMpApplication.java
+++ wechat-mp-autoconfigure/src/main/java/com/github/hippoom/wechat/mp/autoconfigure/annotation/EnableWeChatMpApplication.java
@@ -10,6 +10,27 @@ import java.lang.annotation.Target;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ * Enable WeChat MP support.
+ *
+ * <pre class="code">
+ *    import com.github.hippoom.wechat.mp.autoconfigure.annotation.EnableWeChatMpApplication;
+ *    import org.springframework.boot.SpringApplication;
+ *    import org.springframework.boot.autoconfigure.SpringBootApplication;
+ *
+ *    &#064;EnableWeChatMpApplication
+ *    &#064;SpringBootApplication
+ *    public class Application {
+ *
+ *        public static void main(String[] args) {
+ *            SpringApplication.run(Application.class, args);
+ *        }
+ *    }
+ * </pre>
+ *
+ * @see WeChatMpConfiguration
+ * @since 0.1.0
+ */
 @Retention(RUNTIME)
 @Target(
     {
