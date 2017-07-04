7313daa1049d83bc965e6164b71822aeafe18437
hippoom <hippoom@gmail.com>
2017-07-04 12:03:18 +0800

    Attempt to fix https://travis-ci.org/Hippoom/wechat-mp-starter/builds/249849130

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_95de03115781_key -iv $encrypted_95de03115781_iv -in cd/codesigning.asc.enc -out cd/signingkey.asc -d
 gpg --fast-import cd/signingkey.asc
-mvn deploy -P docs -P ossrh --projects !wechat-mp-starter-demo --settings cd/mvn.settings.xml
+./mvnw deploy -P docs -P ossrh \
+    --projects !examples/core \
+    --projects !examples/oauth2 \
+    --settings cd/mvn.settings.xml
